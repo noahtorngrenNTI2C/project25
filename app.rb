61b6781e8177de95f55afa04f357b0d3ba1b778b
noahtorngrenNTI2C <noah.torngren@elev.ga.ntig.se>
2025-04-08 16:45:27 +0200

    2025-04-08

--- app.rb
+++ app.rb
@@ -29,27 +29,31 @@ get('/quiz/index') do
     slim(:"quiz/index", locals:{quizes: result})
 end
 
-get('/quiz/:id') do
+get('/quiz/show/:id') do
+    p session
     db = SQLite3::Database.new("db/main_database.db")
     db.results_as_hash = true
     result = db.execute("SELECT * FROM quizes WHERE id = ?", params[:id])
-    
-    slim(:"quiz/show", locals:{quiz: result.first})
+    owner_name = db.execute("SELECT * FROM users WHERE id = ?", result.first["owner_id"]).first["username"]
+    questions = db.execute("SELECT questions.name FROM quz_ques INNER JOIN questions ON quz_ques.ques_id = questions.id WHERE quz_ques.quiz_id = ?", params[:id])
+    puts questions
+
+    slim(:"quiz/show", locals:{quiz: result.first, owner: owner_name, questions: questions})
 
 end
 
 # ska fixas
 post('quizes') do
-    title = params[:title]
-    artist_id = params[:artist_id].to_i
-    db = SQLite3::Database.new("db/main_database.db")
-    db.execute("INSERT INTO albums (Title, ArtistId) VALUES (?, ?)",[title,artist_id])
-    redirect('/albums')
+    
 end
 
 get('/quiz/:id/edit') do
 
 
+end
+
+get('/quiz/new') do
+    slim(:"quiz/new")
 end
 
 post('/login') do
@@ -96,4 +100,57 @@ post('/users/new') do
       p "nej"
       "lösernorden matchade inte inte"
     end
-  end
+end
+
+get('/error') do
+    slim(:error, locals:{error_message: "hej"})
+end
+
+before("/quiz/new") do
+    puts "hej"
+    puts !session[:user_id]
+    if !session[:user_id]
+        redirect('/error')
+        #locals:{error_message: "Du måste vara inloggad för att skapa en quiz"}
+    end
+end
+
+#H2 Svara på frågorna nedan    
+#form action="quiz_answer" method="POST"
+#    -questions.each do |question|
+#        p #{question["name"]}
+#        input type="text" name="#{question["name"]}" placeholder="Skriv svar"
+
+post('/quiz/show/quiz_answer') do
+    db = SQLite3::Database.new("db/main_database.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM quizes WHERE id = ?", params[:quiz_id])
+    owner_name = db.execute("SELECT * FROM users WHERE id = ?", result.first["owner_id"]).first["username"]
+    questions = db.execute("SELECT questions.name FROM quz_ques INNER JOIN questions ON quz_ques.ques_id = questions.id WHERE quz_ques.quiz_id = ?", params[:quiz_id])
+    answers = db.execute("SELECT questions.answer FROM quz_ques INNER JOIN questions ON quz_ques.ques_id = questions.id WHERE quz_ques.quiz_id = ?", params[:quiz_id])
+    
+
+    responses = []
+    questions.each do |question|
+        response = params[question["name"].to_sym]
+        responses << response
+    end
+
+    # se vilka svar som är rätt och fel och spara i sessionen
+
+    answers.each_with_index do |answer,index|
+        if responses[index].to_s == answers[index]["answer"].to_s
+            puts "rätt svar"
+            session[answers[index]["answer"].to_s.to_sym] = true
+        else
+            puts "fel svar"
+            session[answers[index]["answer"].to_s.to_sym] = false
+        end
+    end
+
+    session[("has_answered" + params[:quiz_id].to_s).to_sym] = true
+    # skicka tillbaka svaren till quiz/show och visa vilka som var rätt
+
+    redirect("/quiz/show/#{params[:quiz_id]}")
+
+end
