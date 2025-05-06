72dace9593e5a65a4c19bdab0d7b16d8f755034b
noahtorngrenNTI2C <noah.torngren@elev.ga.ntig.se>
2025-05-07 00:01:07 +0200

    slut

--- app.rb
+++ app.rb
@@ -30,13 +30,11 @@ get('/quiz/index') do
 end
 
 get('/quiz/show/:id') do
-    p session
     db = SQLite3::Database.new("db/main_database.db")
     db.results_as_hash = true
     result = db.execute("SELECT * FROM quizes WHERE id = ?", params[:id])
     owner_name = db.execute("SELECT * FROM users WHERE id = ?", result.first["owner_id"]).first["username"]
     questions = db.execute("SELECT questions.name FROM quz_ques INNER JOIN questions ON quz_ques.ques_id = questions.id WHERE quz_ques.quiz_id = ?", params[:id])
-    puts questions
 
     slim(:"quiz/show", locals:{quiz: result.first, owner: owner_name, questions: questions})
 
@@ -132,7 +130,7 @@ post('/quiz/show/quiz_answer') do
 
     responses = []
     questions.each do |question|
-        response = params[question["name"].to_sym]
+        response = params[question["name"].to_sym] || ""
         responses << response
     end
 
@@ -141,10 +139,14 @@ post('/quiz/show/quiz_answer') do
     answers.each_with_index do |answer,index|
         if responses[index].to_s == answers[index]["answer"].to_s
             puts "rätt svar"
-            session[answers[index]["answer"].to_s.to_sym] = true
+            #nyckeln ska inte vara svaret utan frågan
+            
+            session[questions[index]["name"].to_s.to_sym] = true
+            #session[answers[index]["answer"].to_s.to_sym] = true
         else
-            puts "fel svar"
-            session[answers[index]["answer"].to_s.to_sym] = false
+            puts questions[index]["name"].to_s.to_sym
+            session[questions[index]["name"].to_s.to_sym] = false
+            #session[answers[index]["answer"].to_s.to_sym] = false
         end
     end
 
@@ -153,4 +155,84 @@ post('/quiz/show/quiz_answer') do
 
     redirect("/quiz/show/#{params[:quiz_id]}")
 
+end
+
+post('/quiz/qustions') do
+    db = SQLite3::Database.new("db/main_database.db")
+    db.results_as_hash = true
+   
+    #Variabler 
+    quiz_name = params[:quiz_name]
+    
+    #frågor
+    qustion1 = params[:qustion1]
+    qustion2 = params[:qustion2]
+    qustion3 = params[:qustion3]
+    qustion4 = params[:qustion4]
+    qustion5 = params[:qustion5]
+    qustion6 = params[:qustion6]
+
+    #svar
+    answer1 = params[:answer1]
+    answer2 = params[:answer2]
+    answer3 = params[:answer3]
+    answer4 = params[:answer4]
+    answer5 = params[:answer5]
+    answer6 = params[:answer6]
+    
+
+
+    user_id = session[:user_id]
+    #skapa quiz
+    db.execute('INSERT INTO quizes (name, owner_id) VALUES (?,?)',[quiz_name,user_id])
+    
+    #hämta quiz_id
+    quiz_id = db.execute('SELECT id FROM quizes WHERE name = ? AND owner_id = ?', [quiz_name,user_id]).first["id"]
+    for i in 1..6 do
+        # skapa frågor och svar
+        #frågor
+        db.execute('INSERT INTO questions (name, answer) VALUES (?,?)',[params["qustion" + i.to_s],params["answer" + i.to_s]])
+        
+        #hämta fråg_id
+        ques_id = db.execute('SELECT id FROM questions WHERE name = ? AND answer = ?', [params["qustion" + i.to_s],params["answer" + i.to_s]]).first["id"]
+        #quz_ques
+        db.execute('INSERT INTO quz_ques (quiz_id, ques_id) VALUES (?,?)',[quiz_id,ques_id])
+    end
+
+
+    redirect('/quiz/index')
+end
+
+get('/quiz/delete/:id') do
+    if params[:id] == "nil"
+        user_id = session[:user_id]
+        if user_id
+            db = SQLite3::Database.new("db/main_database.db")
+            db.results_as_hash = true
+            result = db.execute("SELECT * FROM quizes WHERE owner_id = ?", user_id)
+            slim(:"quiz/delete", locals:{quizes: result})
+            
+        else
+            redirect('/error')
+        end
+    else
+        db = SQLite3::Database.new("db/main_database.db")
+        db.results_as_hash = true
+        
+        # ta fram frågorna som tillhör quizet
+        questions = db.execute("SELECT questions.id FROM quz_ques INNER JOIN questions ON quz_ques.ques_id = questions.id WHERE quz_ques.quiz_id = ?", params[:id])
+
+        # ta fram quz_ques som tillhör quizet
+        quz_ques = db.execute("SELECT * FROM quz_ques WHERE quiz_id = ?", params[:id])
+
+        questions.each do |question|
+            db.execute("DELETE FROM questions WHERE id = ?", question["id"])
+        end
+
+        quz_ques.each do |quz|
+            db.execute("DELETE FROM quz_ques WHERE quiz_id = ? AND ques_id = ?", [params[:id], quz["ques_id"]])
+        end
+        db.execute("DELETE FROM quizes WHERE id = ?", params[:id])
+        redirect('/quiz/index')
+    end
 end
